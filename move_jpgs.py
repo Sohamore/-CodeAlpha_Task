9e07a6bf55e9fee2e9e2d008284dbce5028d867b
Sohamore <167452259+Sohamore@users.noreply.github.com>
2025-06-10 00:02:23 +0530

    Update move_jpgs.py

--- move_jpgs.py
+++ move_jpgs.py
@@ -7,12 +7,10 @@ destination_folder = r'C:\Users\Soham\OneDrive\Desktop\organize_images'
 print("Source folder exists:", os.path.exists(source_folder))
 print("Destination folder exists:", os.path.exists(destination_folder))
 
-# Create destination folder if it doesn't exist
 if not os.path.exists(destination_folder):
     os.makedirs(destination_folder)
     print("Created destination folder.")
 
-# Move all .jpg or .jpeg files
 for file_name in os.listdir(source_folder):
     print("Found file:", file_name)
     if file_name.lower().endswith(('.jpg', '.jpeg', '.png', '.aivf')):
